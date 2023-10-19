da64bf68c689e5c91b411f63c268984bc9ad1f18
ii <i.broshchak@gmail.com>
2023-10-19 15:26:01 +0300

    Add: add phone and address to users_table

--- database/migrations/2023_10_19_122438_add_address_to_users_table.php
+++ database/migrations/2023_10_19_122438_add_address_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('phone');
+            $table->dropColumn('address');
         });
     }
 };
