efea8fcf7df75392ab9124fa3112ed579bbfbf1e
ii <i.broshchak@gmail.com>
2023-10-19 15:33:48 +0300

    Add: Users Resource | Request | Model

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ReceiverResource extends JsonResource
+class UserResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -15,10 +15,12 @@ class ReceiverResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'full_name' => $this->full_name,
-            'phone_number' => $this->phone_number,
+            'id' => $this->id,
+            'name' => $this->name,
             'email' => $this->email,
-            'delivery_address' => $this->delivery_address,
+            'password' => $this->password,
+            'phone' => $this->phone,
+            'address' => $this->address,
         ];
     }
 }
